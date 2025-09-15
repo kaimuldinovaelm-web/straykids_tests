bfb9249308b6d62768ddc2653e4caab6e83f467d
Elmira Kaimuldinova <kaimuldinovaelm@gmail.com>
2025-09-15 17:32:47 +0700

    add discography tests

--- tests/pages/DiscographyPage.ts
+++ tests/pages/DiscographyPage.ts
@@ -1,16 +1,36 @@
 import { expect, Locator, Page } from '@playwright/test';
 import { BasePage } from './BasePage';
 export class DiscographyPage extends BasePage {
+  private readonly titleLocator: Locator;
   private readonly discographyPageLocator: Locator;
+  private readonly newReleasesLocator: Locator;
+  private readonly nextButtonLocator: Locator;
+  private readonly prevButtonLocator: Locator;
 
   constructor(page: Page) {
     super(page);
+    this.titleLocator = this.page.locator('div').filter({ hasText: /^DISCOGRAPHY$/ });
     this.discographyPageLocator = this.page.locator('.container');
+    this.newReleasesLocator = this.page.locator('.new_release_list');
+    this.nextButtonLocator = this.page.getByRole('button', { name: '이후이후' });
+    this.prevButtonLocator = this.page.getByRole('button', { name: '이전이전' });
   }
   async open() {
     await this.page.goto('https://straykids.jype.com/Default/DiscographyList');
   }
+  async pageHasCorrectTitle() {
+    await expect(this.titleLocator).toHaveText('DISCOGRAPHY');
+  }
   async discigraphyPageHasCorrectLayout() {
     await this.checkLayoutByScreenshot(this.discographyPageLocator, 'discographyPage.png');
+  }
+  async pageHasNewReleasesList() {
+    await expect(this.newReleasesLocator).toBeVisible();
+  }
+  async pageHasNextButton() {
+    await this.nextButtonLocator.click();
+  }
+  async pageHasPrevButton() {
+    await this.prevButtonLocator.click();
   }
 }
--- tests/specs/discographyPage.spec.ts
+++ tests/specs/discographyPage.spec.ts
@@ -1,5 +1,18 @@
 import { test } from '../fixtures/fixtures';
+import { DiscographyPage } from '../pages/DiscographyPage';
 
+test('Correct title test', async ({ discographyPage }) => {
+  await discographyPage.pageHasCorrectTitle();
+});
 test('Discography layout test', async ({ discographyPage }) => {
   await discographyPage.discigraphyPageHasCorrectLayout();
+});
+test('New releases list test', async ({ discographyPage }) => {
+  await discographyPage.pageHasNewReleasesList();
+});
+test('Next button test', async ({ discographyPage }) => {
+  await discographyPage.pageHasNextButton();
+});
+test('Prev button test', async ({ discographyPage }) => {
+  await discographyPage.pageHasPrevButton();
 });
